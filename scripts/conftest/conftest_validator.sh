abe0f14279b747bfc86892d03fc10f5290378c2f
varun-sriramulu <60422477+varun-sriramulu@users.noreply.github.com>
2024-04-08 14:31:38 +0530

    Update conftest_validator.sh

--- scripts/conftest/conftest_validator.sh
+++ scripts/conftest/conftest_validator.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
 
-echo "aaaaaaaa: ${changedFiles}"
+echo "List of changed files: ${changedFiles}"
 for file in ${changedFiles}; do 
-            echo "$file was modified. Directory: $(dirname "$file"), Name: $(basename "$(dirname "$file")")"
+            # echo "$file was modified. Directory: $(dirname "$file"), Name: $(basename "$(dirname "$file")")"
+
+            parentDirPath = $(dirname "$file")
+            parentDirName = $(basename "$(dirname "$file")")
+            
+            echo "$file was modified. Path: $parentDirPath, Name: $parentDirName"
             
             # if [] || []; then
             #   echo "changes_exist=true" >> $GITHUB_ENV
